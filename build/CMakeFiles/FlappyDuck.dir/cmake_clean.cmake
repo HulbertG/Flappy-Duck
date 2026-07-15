a8c340d256bbf435f35cbf6ded653bc4ce760491
HulbertG <hulbertg@email.sc.edu>
2026-07-15 16:03:11 -0400

    Added extra folders

---
+++ build/CMakeFiles/FlappyDuck.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FlappyDuck.dir/Game.cpp.obj"
+  "CMakeFiles/FlappyDuck.dir/Game.cpp.obj.d"
+  "FlappyDuck.exe"
+  "FlappyDuck.exe.manifest"
+  "FlappyDuck.pdb"
+  "libFlappyDuck.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/FlappyDuck.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
